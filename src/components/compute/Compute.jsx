7f6432fd910f16c363149e6d014fdcad528cefe5
wanglei62 <wanglei62@lenovo.com>
2020-06-10 19:05:04 +0800

    Update Compute.jsx

--- src/components/compute/Compute.jsx
+++ src/components/compute/Compute.jsx
@@ -2,38 +2,39 @@ import React from 'react';
 
 
 
-const Compute = function({data, onPlus, onSubstract }){
-  return (
-    <div>
-      {JSON.stringify(data)}<br/>
-      我今年 <span style={{fontSize:14,fontWeight:"bold"}}>{data.i}</span> 岁
-      <input type="button" value="好好吃饭了" onClick={onPlus}/>
-      <input type="button" value="没有好好吃饭" onClick={onSubstract}/>
-    </div>
-  )
-}
-
-export default Compute
-// class Compute extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {}
-//   }
-
-//   render(){
-//     return (
-//       <div>
-//         {JSON.stringify(this.props.data)}<br/>
-
-//         我今年 <span style={{fontSize:14,fontWeight:"bold"}}>{this.props.data.i}</span> 岁
-
-//         <input type="button" value="好好吃饭了" onClick={this.props.plus}/>
-        
-//         <input type="button" value="没有好好吃饭" onClick={this.props.substract}/>
-//       </div>
-//     )
-//   }
+// const Compute = function({data, onPlus, onSubstract }){
+//   return (
+//     <div>
+//       {JSON.stringify(data)}<br/>
+//       我今年 <span style={{fontSize:14,fontWeight:"bold"}}>{data.i}</span> 岁
+//       <input type="button" value="好好吃饭了" onClick={onPlus}/>
+//       <input type="button" value="没有好好吃饭" onClick={onSubstract}/>
+//     </div>
+//   )
 // }
 
-
 // export default Compute
+
+
+
+
+class Compute extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div style={{marginLeft:10}}>
+        {JSON.stringify(this.props.data)}<br/>
+        我今年 <span style={{fontSize:14,fontWeight:"bold"}}>{this.props.data.i}</span> 岁
+        <input type="button" value="+" onClick={this.props.onPlus}/>
+        <input type="button" value="-" onClick={this.props.onSubstract}/>
+      </div>
+    )
+  }
+}
+
+
+export default Compute
